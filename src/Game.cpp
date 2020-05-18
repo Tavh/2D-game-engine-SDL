5ed3e2b5c2e5292a7362b5c5badd38fe17a8cd88
tav <tavherzlich26@gmail.com>
2020-05-19 00:19:47 +0300

    init

---
+++ src/Game.cpp
@@ -0,0 +1,98 @@
+#include <iostream>
+#include "./Constants.h"
+#include "./Game.h"
+
+Game::Game() {
+    this->isRunning = false;
+}
+
+Game::~Game() {}
+
+bool Game::IsRunning() const {
+    return this->isRunning;
+}
+
+float projectilePosX = 0.0f;
+float projectilePosY = 0.0f;
+float projectileVelocityX = 0.1f;
+float projectileVelocityY = 0.1f;
+
+void Game::Initialize(int width, int height) {
+    if (SDL_Init(SDL_INIT_EVERYTHING) != 0) {
+        std::cerr << "Error initializing SDL." << std::endl;
+        return;
+    }
+    this->window = SDL_CreateWindow(
+        NULL, 
+        SDL_WINDOWPOS_CENTERED, 
+        SDL_WINDOWPOS_CENTERED,
+        width,
+        height,
+        SDL_WINDOW_BORDERLESS
+    );
+    
+    if (!window) {
+        std::cerr << "Error creating SDL window." << std::endl;
+        return;
+    }
+
+    const int DEFAULT_DRIVER_INDEX = -1; // Driver basically means monitor in this contex
+    const int NO_FLAGS_INDICATION = 0;
+    this->renderer = SDL_CreateRenderer(window, DEFAULT_DRIVER_INDEX, NO_FLAGS_INDICATION);
+
+    if (!renderer) {
+        std::cerr << "Error creating SDL renderer." << std::endl;
+        return;
+    }
+
+    this->isRunning = true;
+    return;
+}
+
+void Game::ProcessInput() {
+    SDL_Event event;
+    SDL_PollEvent(&event);
+    switch(event.type) {
+        case SDL_QUIT: 
+            this->isRunning = false;
+            break;
+
+        case SDL_KEYDOWN: 
+                if (event.key.keysym.sym == SDLK_ESCAPE) {
+                    this->isRunning = false;
+                    break;
+                }
+        
+
+        default:
+            break;
+    }
+}
+
+void Game::Update() {
+    projectilePosX += projectileVelocityX;
+    projectilePosY += projectileVelocityY;
+}
+
+void Game::Render() {
+    SDL_SetRenderDrawColor(this->renderer, 21, 21, 21, 255);
+    SDL_RenderClear(this->renderer);
+
+    SDL_Rect projectile {
+        (int) projectilePosX,
+        (int) projectilePosY,
+        10,
+        10
+    };
+
+    SDL_SetRenderDrawColor(this->renderer, 255, 255, 255, 255);
+    SDL_RenderFillRect(this->renderer, &projectile);
+
+    SDL_RenderPresent(this->renderer);
+}
+
+void Game::Destroy() {
+    SDL_DestroyRenderer(this->renderer);
+    SDL_DestroyWindow(this->window);
+    SDL_Quit();
+}
