fd0178bc5ea5a02c29c496fa1dc44f589c8b10f4
tav <tavherzlich26@gmail.com>
2020-05-20 02:22:06 +0300

    started entity-component architecture

---
+++ src/EntityManager.cpp
@@ -0,0 +1,37 @@
+#include "EntityManager.h"
+
+void EntityManager::ClearData() {
+    for (auto& entity : this->entities) {
+        entity->Destroy();
+    }
+}
+
+bool EntityManager::HasNoEntities() const {
+    return this->entities.size() == 0;
+}
+
+void EntityManager::Update(float deltaTime) {
+    for (auto& entity : this->entities) {
+        entity->Update(deltaTime);
+    }
+}
+
+void EntityManager::Render() {
+    for (auto& entity : this->entities) {
+        entity->Render();
+    }
+}
+
+Entity& EntityManager::AddEntity(std::string entityName) {
+    Entity* entity = new Entity(*this, entityName);
+    entities.emplace_back(entity);
+    return *entity;
+}
+
+std::vector<Entity*> EntityManager::GetEntities() const {
+    return this->entities;
+}
+
+unsigned int EntityManager::GetEntityCount() {
+    return this->entities.size();
+}
