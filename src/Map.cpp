b59d45ec5130632f6cc0d9bb35e00a8bc9c20f5b
Tav <tavherzlich26@gmail.com>
2020-05-21 14:38:59 +0300

    Implemented map and tiles

---
+++ src/Map.cpp
@@ -0,0 +1,36 @@
+#include <fstream>
+#include "Map.h"
+#include "./Game.h"
+#include "./EntityManager.h"
+#include "./components/TileComponent.h"
+
+extern EntityManager manager;
+
+Map::Map(std::string textureId, int scale, int tileSize) {
+    this->textureId = textureId;
+    this->scale = scale;
+    this->tileSize = tileSize;
+}
+
+void Map::LoadMap(std::string filePath, int mapSizeX, int mapSizeY) {
+    std::fstream mapFile;
+    mapFile.open(filePath);
+
+    for (int y = 0; y < mapSizeY; y++) {
+        for (int x = 0; x < mapSizeX; x++) {
+            char ch;
+            mapFile.get(ch);
+            int srcRectY = atoi(&ch) * tileSize;
+            mapFile.get(ch);
+            int srcRectX = atoi(&ch) * tileSize;
+            AddTile(srcRectX, srcRectY, x * (scale * tileSize), y * (scale * tileSize));
+            mapFile.ignore();
+        }
+    }
+    mapFile.close();
+}
+
+void Map::AddTile(int srcRectX, int srcRectY, int x, int y) {
+    Entity& newTile(manager.AddEntity("Tile"));
+    newTile.AddComponent<TileComponent>(srcRectX, srcRectY, x, y, this->tileSize,this->scale, this->textureId);
+}
---
+++ src/components/TileComponent.h
@@ -0,0 +1,50 @@
+#ifndef TILECOMPONENT_H
+#define TILECOMPONENT_H
+
+#include <SDL2/SDL.h>
+#include "../../lib/glm/glm.hpp"
+#include "../EntityManager.h"
+#include "../AssetManager.h"
+
+class TileComponent : public Component {
+public:
+    SDL_Texture* texture;
+    SDL_Rect srcRect;
+    SDL_Rect destRect;
+    glm::vec2 position;
+
+    TileComponent(int srcRectX, 
+                  int srcRectY, 
+                  int x, 
+                  int y, 
+                  int tileSize, 
+                  int tileScale, 
+                  std::string assetTextureId) {
+        texture = Game::assetManager->GetTexture(assetTextureId);
+        srcRect.x = srcRectX;
+        srcRect.y = srcRectY;
+        srcRect.w = tileSize;
+        srcRect.h = tileSize;
+        
+        destRect.x = x;
+        destRect.y = y;
+        destRect.w = tileSize * tileScale;
+        destRect.h = tileSize * tileScale;
+
+        position.x = x;
+        position.y = y;
+    }
+
+    ~TileComponent() {
+        SDL_DestroyTexture(texture);
+    }
+
+    void Update(float deltaTime) override {
+        // TODO: This is where we'll take care of the tile positions based on the camera control that will keep moving around
+    }
+
+    void Render() override {
+        TextureManager::Draw(texture, srcRect, destRect, SDL_FLIP_NONE);
+    }
+};
+#endif
